0f8ef486f928c0f3477ea89bb942b2ecd070254c
Faduma <fadumascode@gmail.com>
2023-04-25 21:00:14 +1000

    Added escape function - close modal

--- starter/script.js
+++ starter/script.js
@@ -19,5 +19,10 @@ for (let i= 0; i < btnsOpenModal.length; i++)
     btnsOpenModal[i].addEventListener('click', openModal);
     btnCloseModal.addEventListener('click', closeModal);
     overlay.addEventListener('click', closeModal);
-
+    document.addEventListener('keydown',function(e){
+        if (e.key === 'Escape' && !modal.classList.contains('hidden')){
+            closeModal();
+        }
+    
+    });
 
